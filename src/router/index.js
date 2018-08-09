f885a3292a09a9c43a5e72b7bfc5239b7a81d4bc
邵龙 <15688885988@163.com>
2018-08-09 14:40:28 +0800

    fix eslint相关； 新增axios demo示例；

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import GoodsList from './../view/GoodsList';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods/:goodsId/user/:userId',
+      name: 'GoodsList',
+      component: GoodsList
     }
   ]
 });
