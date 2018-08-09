7d19456bc10ba112290ea193707326d76d9deaf8
邵龙 <15688885988@163.com>
2018-08-09 14:50:16 +0800

    fix eslint相关； 新增axios demo示例；

---
+++ src/view/GoodsList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 class="title">{{title}}</h2>
+    <h4>{{$route.params.goodsId}}</h4>
+    <h4>{{$route.params.userId}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoodsList',
+  data () {
+    return {
+      title: '商品列表页面'
+    };
+  }
+};
+</script>
+
+<style scoped>
+  .title{
+    color:red;
+  }
+</style>
